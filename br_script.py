cb377a6827900241851c5490688e6c493af348d0
Vaibhav Bhandari <vaibhavb@gmail.com>
2019-12-02 23:17:07 -0800

    code update

--- br_script.py
+++ br_script.py
@@ -1,3 +1,4 @@
+# *- BSD *-
 from aiy.board import Board, Led
 from aiy.voice.audio import AudioFormat, play_wav, record_file
 import random 
@@ -10,16 +11,14 @@ def main():
             print('ON')
             board.led.state = Led.ON
             board.button.wait_for_release()
-            # Generates a random number between 
-            # a given positive range 
+            # Generates a random number between a given positive range
             r1 = random.randint(1, 6) 
             print("Random number between 1 and 6 is % s" % (r1)) 
-            wav = "/home/pi/breatheresponsibly/%s.wav" % r1
+            wav = "/home/pi/breatheresponsibly/sounds/%s.wav" % r1
             print(wav)
             play_wav(wav)
             print('OFF')
             board.led.state = Led.OFF
-
 
 if __name__ == '__main__':
     print("Starting")
